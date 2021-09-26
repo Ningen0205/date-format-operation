9ec960c9328de83763ea62d4060b8c81ff204356
Ningen <msg1801022@gmail.com>
2021-09-26 21:22:59 +0900

    関数をいろいろ追加+テスト追加

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,72 @@
 export class DateFormatOperation{
   
   private date: Date
+  private dayOfWeekArr: string[]
   
-  constructor(date: Date){
+  constructor(
+    date: Date, 
+    dayOfWeekArr: string[] = ["(日)","(月)","(火)","(水)","(木)","(金)","(土)"]
+  ){
     this.date = date;
+    this.dayOfWeekArr = dayOfWeekArr;
   }
 
   getDate(): Date {
     return this.date;
   }
 
+  setDate(date: Date): void {
+    this.date = date;
+  }
+
   /*
         Get formatted Dates
         - getFormattedDay()
         - getFormattedHours()
-    */
+  */
 
+  getFormattedFullDate(): string {
+    const year = this.date.getFullYear();
+    const month = this.getFormattedMonth();
+    const day = this.getFormattedDay();
+    const hours = this.getFormattedHours();
+    const minutes = this.getFormattedMinutes();
+    const seconds = this.getFormattedSeconds();
+    const dayOfWeek = this.getDayOfWeek();
+
+    return `${year}/${month}/${day}${dayOfWeek} ${hours}:${minutes}:${seconds}`;
+  }
+
+  getFormattedMonth(): string {
+    return ("0"+(this.date.getMonth()+1)).slice(-2);
+  }
 
   getFormattedDay(): string {
     return ("0"+this.date.getDate()).slice(-2);
   }
 
   getFormattedHours(): string {
-    return ("0"+(this.date.getHours() + 1)).slice(-2);
+    return ("0"+(this.date.getHours())).slice(-2);
+  }
+
+  getFormattedMinutes(): string {
+    return ("0"+(this.date.getMinutes())).slice(-2);
+  }
+
+  getFormattedSeconds(): string {
+    return ("0"+(this.date.getSeconds())).slice(-2);
+  }
+
+  getDayOfWeek():string  {
+    return this.dayOfWeekArr[this.date.getDay()];
   }
 
 
   /*
-        Year operating functions 
+      Year operating functions 
         - setBeforeYear()
         - setNextYear()
-    */
+  */
 
   setBeforeYear(): void {
     this.date.setFullYear(this.date.getFullYear() - 1);
--- test/src/index.test.ts
+++ test/src/index.test.ts
@@ -2,25 +2,71 @@ import { DateFormatOperation } from "../../src/index";
 
 describe("Test DateFormatOperation", () => {
   let d: Date;
+  const dayOfWeek = ["(日)","(月)","(火)","(水)","(木)","(金)","(土)"];
   let dateFormatOperation: DateFormatOperation;
-  beforeEach(() => {
-    d = new Date(2000,1,1,0,0,0);
-    dateFormatOperation = new DateFormatOperation(d);
 
+  beforeEach(() => {
+    d = new Date("2000/1/1 0:0:0");
+    dateFormatOperation = new DateFormatOperation(d, dayOfWeek);
   });
 
   test("Test getDate()", () => {
-    const result = new Date(2000,1,1,0,0,0);
+    const result = new Date("2000/1/1 0:0:0");
     expect(dateFormatOperation.getDate()).toStrictEqual(result);
   });
 
+  test("Test setDate()", () => {
+    const dateValue = new Date("2021/1/1 0:0:0");
+    dateFormatOperation.setDate(dateValue);
+
+    expect(dateFormatOperation.getDate()).toBe(dateValue);
+  });
+
+  describe("dayOfWeek()", () => {
+    test("default Value", () => {
+      dateFormatOperation = new DateFormatOperation(d)
+      expect(dateFormatOperation.getDayOfWeek()).toBe("(土)")
+    })
+
+    test("arrange Value", () => {
+      dateFormatOperation = new DateFormatOperation(d, ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"])
+      expect(dateFormatOperation.getDayOfWeek()).toBe("Sat")
+    })
+  })
+
+
   describe("Format", () => {
-    test("Test FormattedDay()", () => {
+
+    test("Test getFormattedMonth()", () => {
+      expect(dateFormatOperation.getFormattedMonth()).toBe("01");
+    });
+    
+    test("Test getFormattedDay()", () => {
       expect(dateFormatOperation.getFormattedDay()).toBe("01");
     });
     
-    test("Test FormattedHours()", () => {
-      expect(dateFormatOperation.getFormattedHours()).toBe("01");
+    test("Test getFormattedHours()", () => {
+      expect(dateFormatOperation.getFormattedHours()).toBe("00");
+    });
+
+    test("Test getFormattedMinutes()", () => {
+      expect(dateFormatOperation.getFormattedMinutes()).toBe("00");
+    });
+
+    test("Test getFormattedMinutes()", () => {
+      expect(dateFormatOperation.getFormattedMinutes()).toBe("00");
+    });
+
+    test("Test getFormattedSeconds()", () => {
+      expect(dateFormatOperation.getFormattedSeconds()).toBe("00");
+    });
+
+    test("Test getDayOfWeek()", () => {
+      expect(dateFormatOperation.getDayOfWeek()).toBe(dayOfWeek[6]);
+    });
+
+    test("Test getFormattedFullDate()", () => {
+      expect(dateFormatOperation.getFormattedFullDate()).toBe("2000/01/01(土) 00:00:00");
     });
   });
 
@@ -29,13 +75,13 @@ describe("Test DateFormatOperation", () => {
   describe("Year", () => {
     test("Test setBeforeYear()", () => {
       dateFormatOperation.setBeforeYear();
-      const result = new Date(1999,1,1,0,0,0);
+      const result = new Date("1999/1/1 0:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
     
     test("Test setNextYear()", () => {
       dateFormatOperation.setNextYear();
-      const result = new Date(2001,1,1,0,0,0);
+      const result = new Date("2001/1/1 0:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
   });
@@ -44,13 +90,13 @@ describe("Test DateFormatOperation", () => {
   describe("Month", () => {
     test("Test setBeforeMonth()", () => {
       dateFormatOperation.setBeforeMonth();
-      const result = new Date(1999,12,1,0,0,0);
+      const result = new Date("1999/12/1 0:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
     
     test("Test setNextMonth()", () => {
       dateFormatOperation.setNextMonth();
-      const result = new Date(2000,2,1,0,0,0);
+      const result = new Date("2000/2/1 0:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
   });
@@ -60,13 +106,13 @@ describe("Test DateFormatOperation", () => {
   describe("Day", () => {
     test("Test setBeforeDay()", () => {
       dateFormatOperation.setBeforeDay();
-      const result = new Date(1999,12,31,0,0,0);
+      const result = new Date("1999/12/31 0:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
     
     test("Test setNextDay()", () => {
       dateFormatOperation.setNextDay();
-      const result = new Date(2000,1,2,0,0,0);
+      const result = new Date("2000/1/2 0:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
   });
@@ -76,13 +122,13 @@ describe("Test DateFormatOperation", () => {
   describe("Hours", () => {
     test("Test setBeforeHours()", () => {
       dateFormatOperation.setBeforeHours();
-      const result = new Date(1999,12,31,23,0,0);
+      const result = new Date("1999/12/31 23:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
     
     test("Test setNextHours()", () => {
       dateFormatOperation.setNextHours();
-      const result = new Date(2000,1,1,1,0,0);
+      const result = new Date("2000/1/1 1:0:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
   });
@@ -90,13 +136,13 @@ describe("Test DateFormatOperation", () => {
   describe("Minutes", () => {
     test("Test setBeforeMinutes()", () => {
       dateFormatOperation.setBeforeMinutes();
-      const result = new Date(1999,12,31,23,59,0);
+      const result = new Date("1999/12/31 23:59:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
     
     test("Test setNextMinutes()", () => {
       dateFormatOperation.setNextMinutes();
-      const result = new Date(2000,1,1,0,1,0);
+      const result = new Date("2000/1/1/ 0:1:0");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
   });
@@ -104,13 +150,13 @@ describe("Test DateFormatOperation", () => {
   describe("Seconds", () => {
     test("Test setBeforeSeconds()", () => {
       dateFormatOperation.setBeforeSeconds();
-      const result = new Date(1999,12,31,23,59,59);
+      const result = new Date("1999/12/31 23:59:59");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
 
     test("Test setNexteSeconds()", () => {
       dateFormatOperation.setNextSeconds();
-      const result = new Date(2000,1,1,0,0,1);
+      const result = new Date("2000/1/1 0:0:1");
       expect(dateFormatOperation.getDate()).toStrictEqual(result);
     });
   });
